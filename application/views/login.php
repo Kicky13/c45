67c7d4e71d04504e6f924bbabed413db76194e1f
Kicky13 <32523482+Kicky13@users.noreply.github.com>
2018-02-15 20:41:54 +0700

    20:41

---
+++ application/views/login.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>C45 Website</title>
+</head>
+<body>
+<h4>Selamat Datang di Website Uji Coba C45</h4>
+    <form action="<?php echo base_url(); ?>" method="post">
+        <label>Username</label>
+        <input type="text" name="username">
+        <label>Password</label>
+        <input type="password" name="password">
+        <button type="submit" value="submit">Login</button>
+    </form>
+</body>
+</html>
